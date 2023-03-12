204ec091a1c5a49947a5668e74acc30afa613e48
Mr-ejack <jokinig.really@gmail.com>
2023-03-12 19:23:07 +0500

    changes for heroku
    changes for heroku.

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 
 STATICFILES_DIRS=[
 STATIC_DIR,
